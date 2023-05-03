00b057081390f51d2651ea8ea886944a95a2e642
Ean Hatfield <hatfield.69@wright.edu>
2023-05-03 15:06:05 -0400

    Update dataPlayground.py

--- src/dataPlayground.py
+++ src/dataPlayground.py
@@ -1,3 +1,24 @@
-from cloverapi.cloverapi_client import CloverApiClient
+import requests
 
-print("Starting...")
+from constants import apiKey, merchantID
+apiURL = 'https://apisandbox.dev.clover.com'
+
+def main():
+    print("Starting...")
+    url = f"https://api.clover.com/v3/merchants/{merchantID}"
+
+    headers = {"accept": "application/json"}
+
+    response = requests.get(url, headers=headers)
+
+    print(response.text)
+    
+    
+    
+
+
+
+
+
+if(__name__ == "__main__"):
+    main()
