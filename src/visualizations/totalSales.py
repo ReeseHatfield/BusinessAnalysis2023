cbbc605dd6305c10ed0a72866ded51e6e5f894a7
Ean Hatfield <hatfield.69@wright.edu>
2023-05-06 10:31:08 -0400

    Refactored totalSales.py for better visual

--- src/visualizations/totalSales.py
+++ src/visualizations/totalSales.py
@@ -12,16 +12,24 @@ from collections import OrderedDict
 def main():
     reader = DataReader(os.path.join('dataset', 'dataSet.csv'))
 
-    sales_per_day = OrderedDict()
+    sales_per_day = getSalesPerDay(reader)
+    is_by_weeks = False
+    plotData(sales_per_day, is_by_weeks)
 
+    
+    print(len(sales_per_day))
+
+    
+    
+def getSalesPerDay(reader: DataReader) -> OrderedDict:
+
+    sales_per_day = OrderedDict()
     num_sales_in_day = 0
 
     for i in range(2, reader.getNumRows()):
         
         previousDate = parse(reader.getRow(i-1)[0])
         currentDate = parse(reader.getRow(i)[0])
-      
-
 
         if currentDate.date() == previousDate.date():
             num_sales_in_day += 1
@@ -32,22 +40,19 @@ def main():
     
             sales_per_day[currentDate] = num_sales_in_day
             
-            num_sales_in_day = 0    
-    
-    print(len(sales_per_day))
+            num_sales_in_day = 0   
 
-    days = np.arange(len(sales_per_day))
-    plt.bar(days, list(sales_per_day.values()))
+    return sales_per_day
 
-    
-    
-    
-    plt.xlabel("Time")
+def plotData(data: OrderedDict, is_by_weeks: bool) -> None:
+
+    divider = 4 if is_by_weeks else 1
+
+    days = np.arange(len(data))
+    plt.bar(days / divider, list(data.values()), edgecolor = None)
+    plt.xlabel("Day")
     plt.ylabel("Sales")
-
     plt.show()
-    
-    
 
 
 if __name__ == "__main__":
