3f1eb72278b7525df6e79bea13cdf1907017b31c
Reese Hatfield <hatfield.69@wright.edu>
2023-05-10 21:08:45 -0400

    Finalize Poly Regression

--- src/visualizations/PolynomialRegressionVisual.py
+++ src/visualizations/PolynomialRegressionVisual.py
@@ -11,10 +11,31 @@ sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..',
 from PolynomialRegression import PolynomialRegression
 
 def main():
+    print(os.getcwd)
     reader = DataReader(os.path.join('dataset', 'dataSet.csv'))
+
     sales = reader.getSalesPerDay()
 
-    degree: int = 5
+    #animatePlot(sales)
+
+    domain = range(len(sales))
+    function = sales
+    degree: int = 40
+
+    model = np.poly1d(np.polyfit(domain,function, degree))
+    line = np.linspace(1, len(sales), len(sales))
+    plt.plot(domain, function)
+    plt.plot(line, model(line), color="red")
+    plt.show()
+
+
+    
+    
+
+   
+       
+def animatePlot(sales):
+     
     x = range(len(sales))
     y = sales
 
@@ -27,7 +48,6 @@ def main():
         plt.savefig("degree_" + str(i) + ".png")
        
         plt.clf()
-       
 
 if __name__ == "__main__":
     main()
