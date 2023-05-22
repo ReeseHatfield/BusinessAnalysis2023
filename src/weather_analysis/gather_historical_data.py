5495b347132c1f6078cf52d073d0e0985fea9743
Ean Hatfield <hatfield.69@wright.edu>
2023-05-22 12:31:15 -0400

    Added ability to gather historical data
    Uses the Open Meteo API to get about 600 days of historical weather data, and pickles it to a dict

--- src/weather_analysis/gather_historical_data.py
+++ src/weather_analysis/gather_historical_data.py
@@ -1,13 +1,67 @@
 import os
 import sys
-sys.path.append(os.path.join("src","constants.py"))
+import requests
+import datetime
+import pickle
+
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 from constants import open_weather_key
 
-
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__),'..' ,'..', 'src', 'parsing')))
+from DataReader import DataReader
 
 def main():
-    print(open_weather_key)
+    BASE_URL = "https://archive-api.open-meteo.com/v1/archive"
+    LATITUDE = 39.344538  
+    LONGITUDE = -82.988237  
 
+    pickle_file_path = os.path.join('dataset', 'dates.pkl')
 
+    if os.path.exists(pickle_file_path):
+        print("Using pre-computed data")
+        with open(pickle_file_path, 'rb') as f:
+            dates = pickle.load(f)
+    else:
+        print("Computing data")
+        reader = DataReader(os.path.join('dataset', 'dataSet.csv'))
+        dates = reader.getDates()
+        with open(pickle_file_path, 'wb') as f:
+            pickle.dump(dates, f)
+
+    weather_dict_by_day = {}
+
+    # Define date in ISO format
+    for date in dates:
+        current_date = date.date()
+
+        # Define parameters for the API request
+        request_params = {
+            "latitude": LATITUDE,
+            "longitude": LONGITUDE,
+            "start_date": current_date,
+            "end_date": current_date,
+            "daily": "temperature_2m_mean,precipitation_sum",
+            "timezone": "America/New_York",
+            "temperature_unit": "fahrenheit",
+            "precipitation_unit": "inch"
+        }
+
+        # Send GET request to Open-Meteo API
+        response = requests.get(BASE_URL, params=request_params).json()
+
+        mean_temperature = response['daily']['temperature_2m_mean'][0] 
+        preciptation_sum = response['daily']['precipitation_sum'][0]
+
+        print(f"Day: {current_date}")
+        print(mean_temperature)
+        print(preciptation_sum)
+        weather_dict_by_day[current_date] = (mean_temperature, preciptation_sum)
+
+    # date_to_retrieve = datetime.date(2020, 9, 1)  
+    # weather_data = weather_dict_by_day[date_to_retrieve]    
+    weather_pickle_path = os.path.join('dataset', 'weather_data_by_day.pkl')
+    with open(weather_pickle_path, 'wb') as f:
+        pickle.dump(weather_dict_by_day, f)
+    
 if __name__ == "__main__":
     main()
