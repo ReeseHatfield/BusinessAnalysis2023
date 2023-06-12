66f9f8f2f9c4184434fafa0939562bbbfd2197be
Ean Hatfield <hatfield.69@wright.edu>
2023-06-12 12:40:25 -0400

    published constants file and hide specific data

---
+++ src/constants/constants.py
@@ -0,0 +1,22 @@
+import os
+
+summer_21_dates = [164, 220]
+summer_22_dates = [401, 448]
+
+_weather_effect = 0.081
+
+
+if os.path.exists(os.path.join('src', 'constants', 'business_specific_constants.py')):
+    from src.constants.business_specific_constants import specific_business_weather_effect
+    _weather_effect = specific_business_weather_effect
+
+heavy_precip_effect = _weather_effect * (3 / 3)
+moderate_precip_effect = _weather_effect * (2 / 3)
+light_precip_effect = _weather_effect * (1 / 3)
+
+precip_dict = {
+    'None': 0,
+    'Light': light_precip_effect,
+    'Moderate': moderate_precip_effect,
+    'Heavy': heavy_precip_effect
+}
